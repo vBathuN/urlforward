78d03f49094ce94f4159ed58a03e2313ae702a6c
Luis Daniel Vargas <luisdatec@gmail.com>
2023-05-14 17:49:16 +0000

    Activation of step 3 and spaces

--- index.php
+++ index.php
@@ -1,26 +1,33 @@
 <?php
+  
   /* REQUIRED */
   include 'config.php';
 
   /* 1. Redirect in PHP */
   $redirect_url = REDIRECT_URL;
   header('Location: ' . $redirect_url);
+
 ?>
 
 <!DOCTYPE html>
+
 <html>
+
   <head>
     <?php 
         /* 2. Redirect in PHP + HTML Head */
         echo '<meta http-equiv="refresh" content="0; url=' . $redirect_url . '">'; 
     ?>
   </head>
+
   <body onload="redirect(); show_redirect_url();" >
+    
     <p id="paragraph" style="display:none;">If you're not automatically redirected, <a href="<?php echo $redirect_url; ?>">click here</a>.</p>
+    
     <script>
         /* 3. Redirect in HTML Tag + Javascript */
         function redirect() {
-            //window.location.href = "<?php echo $redirect_url; ?>";
+          window.location.href = "<?php echo $redirect_url; ?>";
         }
 
         /* 4. Redirect in HTML + Javascript + DOM + CSS */
@@ -31,5 +38,7 @@
             }, 1500);
         }
     </script>
+
   </body>
+
 </html>

d7bc8a93c123a107ae0727e38f78400ad6a6fc18
Luis Daniel Vargas <luisdatec@gmail.com>
2023-05-14 11:43:16 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    /* 1. Redirect in PHP */
+    $redirect_url = 'https://sample.com';
+    header('Location: ' . $redirect_url);
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php 
+        /* 2. Redirect in PHP + HTML Head */
+        echo '<meta http-equiv="refresh" content="0; url=' . $redirect_url . '">'; 
+    ?>
+  </head>
+  <body onload="redirect(); show_redirect_url();" >
+    <p id="paragraph" style="display:none;">If you're not automatically redirected, <a href="<?php echo $redirect_url; ?>">click here</a>.</p>
+    <script>
+        /* 3. Redirect in HTML Tag + Javascript */
+        function redirect() {
+            //window.location.href = "<?php echo $redirect_url; ?>";
+        }
+
+        /* 4. Redirect in HTML + Javascript + DOM + CSS */
+        function show_redirect_url(){
+            setTimeout(function() {
+                var paragraph = document.getElementById("paragraph");
+                paragraph.style.display = "block";
+            }, 1500);
+        }
+    </script>
+  </body>
+</html>
